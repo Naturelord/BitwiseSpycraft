6f91d2dbf45e165c477ed23370e76d38f187e3ed
Naturelord <blakepatton13@gmail.com>
2020-02-13 14:26:23 -0800

    Added base class as well as some base images
    added the base classes as well as the base images

---
+++ PPMImage.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+
+public class PPMImage 
+{
+private String magicNumber;
+private int width;
+private int height;
+private int maxColorValue;
+private char[][][] raster;
+private File InputImage;
+public PPMImage(File image)
+{
+	// Validate that the given File is a PPM image
+	
+	// If so Load into memory by assigning to InputImage
+}
+
+public void writeImage(File fileName)
+{
+	
+}
+
+private void readImage()
+{
+	
+}
+
+public void hideData(String message)
+{
+	
+}
+
+public String recoverData()
+{
+	return "";
+}
+
+}
