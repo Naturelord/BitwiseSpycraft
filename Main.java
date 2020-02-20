131e92d07caeb93427ae51247446a14bce17c33c
Naturelord <blakepatton13@gmail.com>
2020-02-20 10:26:59 -0800

    Almost Done
    Almost done just need spaces to be entered in

--- Main.java
+++ Main.java
@@ -1,23 +1,26 @@
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
-
+public class Main 
+{
+	// Global Variables
+	private static String InputFileName;
+	private static String OutPutFileName;
+	private static String Message;
+	private static Scanner in;
 	// PPMIMage class for PPM image manipulation
 	public PPMImage Manipulator;
 	
+	// Main Method
 	public static void main(String[] args)
 	{
-		String InputFileName;
-		String OutPutFileName;
-		String Message;
-		
-		boolean End = false;
-		Scanner in = new Scanner(System.in);
-		
-		
+		// Initialize Scanner
+		in = new Scanner(System.in);
+		// Print BoilerPlate to show Available Options to User
 		PrintBoilerPlate();
+		// Loop Boiler Plate after User finishes a request UNLESS user wishes to end.
 		Boolean end = false;
 		while(!end) 
 		{
@@ -35,32 +38,21 @@ public class Main {
 			{
 				System.exit(-1);
 			}
-			else if(choice.equals("a")) {
-				// Print out request for Input Image File Name - then store the string entered
-				System.out.println("Please Specify the source PPM fileName:");
-				try{InputFileName = in.next();}
-				catch(Exception e){e.printStackTrace();}
-				// Print out request for Desired Output Image File Name - Then store the string entered
-				System.out.println("Please Specify the output PPM fileName:");
-				try{OutPutFileName = in.next();}
-				catch(Exception e){e.printStackTrace();}
-				System.out.println("Please Enter the Phrase to Hide:");
-				try{Message = in.next();}
-				catch(Exception e){e.printStackTrace();}
-				
-				
-				// End with new Boiler Plate
-				PrintBoilerPlate();
+			else if(choice.equals("a"))
+			{
+				EncodeMessage();
 			}
-			else if(choice.equals("b")) {
-				// Do Something
-				
-				// End with new Boiler Plate
-				PrintBoilerPlate();
+			else if(choice.equals("b"))
+			{
+				DecodeMessage();
 			}
 			else if(choice.equals("c")) {
 				end = true;
 				System.out.println("\n\nThank you for using SPYCRAFT\n\n");
+			}
+			else
+			{
+				PrintBoilerPlate();
 			}
 			
 			
@@ -80,6 +72,45 @@ public class Main {
 		
 		System.out.print(Boiler);
 	}
-
+	
+	public static void EncodeMessage()
+	{
+		// Print out request for Input Image File Name - then store the string entered
+		System.out.print("Please Specify the source PPM fileName:");
+		try{InputFileName = in.next();}
+		catch(Exception e){e.printStackTrace();}
+		// Print out request for Desired Output Image File Name - Then store the string entered
+		System.out.print("Please Specify the output PPM fileName:");
+		try{OutPutFileName = in.next();}
+		catch(Exception e){e.printStackTrace();}
+		System.out.print("Please Enter the Phrase to Hide:");
+		Message  = "";
+		Message = in.nextLine();
+		// Create a new PPM Image based on the file information at InputFileName
+		PPMImage Image = new PPMImage(new File(InputFileName));
+		// Hide the Data in the Raster
+		Image.hideData(Message);
+		// Output the new PPM Image based on the File Location OutPutFileName
+		try {
+			Image.writeImage(new File(OutPutFileName));
+		} catch (IOException e) {
+			System.out.println("Could Not Encode The Message into a new PPM");
+			e.printStackTrace();
+		}
+		// End with new Boiler Plate
+		PrintBoilerPlate();
+	}
+	public static void DecodeMessage()
+	{
+		// Request the File Path
+		System.out.print("\nPlease specify the source PPM filename:");
+		try{InputFileName = in.next();}
+		catch(Exception e){e.printStackTrace();}
+		PPMImage In = new PPMImage(new File(InputFileName));
+		// Decode and Display the Message
+		System.out.print("The Message Recovered is: "+In.recoverData());
+		// End with new Boiler Plate
+		PrintBoilerPlate();
+	}
 }
 
--- PPMImage.java
+++ PPMImage.java
@@ -1,41 +1,199 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 
 public class PPMImage 
 {
-private String magicNumber;
-private int width;
-private int height;
-private int maxColorValue;
-private char[][][] raster;
-private File InputImage;
+public String magicNumber;
+public int width;
+public int height;
+public int maxColorValue;
+public char[][][] raster;
+
+public int startLocation;
+
+public byte[] All;
+public byte[] precursorInfo;
+public byte[] info;
+
 
 public PPMImage(File image)
 {
-	// Validate that the given File is a PPM image
-	
+	// Read the Image into the Raster for Future Manipulation
+	readImage(image);
 	// If so Load into memory by assigning to InputImage
 }
 
-public void writeImage(File fileName)
+public void writeImage(File fileName) throws IOException
 {
-	
+	// Transform the raster into new raw data
+	FileOutputStream fos = new FileOutputStream(fileName);
+	for(int writeIndex = 0; writeIndex <startLocation; writeIndex++)
+	{
+		fos.write(All[writeIndex]);
+	}
+	fos.write(info);
 }
 
-private void readImage()
+private void readImage(File file)
 {
-	
+try
+{
+
+byte[] chars = Files.readAllBytes(file.toPath());
+All = chars;
+String s ="";
+
+for(int i=0; i<chars.length && i < 25;i++)
+{
+		s+= (char) chars[i];
+}
+
+String[] values = s.split("\n");
+// Get the Magic Number
+magicNumber =  values[0];
+// Get the Height and Width
+	// First Split Values[1]
+String[] WH = values[1].split(" ");
+	// Then assign based on split
+width  = Integer.parseInt(WH[0]);
+height = Integer.parseInt(WH[1]);
+// Assign MaxValue and Initialize Raster
+raster = new char[height][width][3];
+maxColorValue = Integer.parseInt(values[2]);
+// Find Where StartLocation is Located in chars[]
+int StartLocation = s.indexOf("255")+4;
+startLocation = StartLocation;
+info = new byte[chars.length-StartLocation];
+int index =0;
+for(int set=StartLocation; set<chars.length; set++)
+{
+info[index] = chars[set];
+index++;
+}
+//System.out.print("First Index:"+firstReturnIndex+" SpaceIndex:"+spaceIndex+" secondReturnIndex:"+secondReturnIndex);
+for(int y=0;y<height;y++)
+{
+	for(int x=0;x<width;x++)
+	{
+		for(int i=0;i<3;i++)
+		{
+			raster[y][x][i] = (char) ( chars[y*width*3+x*3+i+StartLocation] & 0xFF);
+		}
+	}
+}
+
+}
+catch(Exception e)
+{
+	System.out.println("Your Scanning in Failed");
+	e.printStackTrace();
+}
 }
 
 public void hideData(String message)
 {
-	// EncodeImage encoder = new EncodeImage(this);
+	message +='\0';
+	// Concat the Ending Byte
+	byte[] bits = new byte[message.length()];
+	for(int i =0; i<message.length();i++)
+	{
+		bits[i] = (byte) message.charAt(i);
+	} 
+	
+	for(int i =0; i<bits.length;i++)// Go through Bits using each byte 8 times
+	{
+		for(int n=1;n<=8;n++)// Check the Nth bit and compare it to the LSD at that location
+		{
+			// LSD location will be at info[i*8 + n-1]
+			char positivemask = (char) (1 << n-1);
+			
+			// if the nth digit of bits is 1 and LSD at info[i*8 + n-1] == 0
+			if(  (bits[i] & positivemask) > 0 && info[(i+1)*8 - n]%2 ==0)
+			{
+				// Then we need to add 1 to info by Or'ing a 0000 0001
+				info[(i+1)*8 - n] = (byte) (info[(i+1)*8 - n] | 0x01);
+				System.out.println("We added a 1 at Index: "+((i+1)*8 - n));
+			}
+			// // if the nth digit of bits is 0 and LSD at info[i*8 + n-1] == 1
+			else if(  (bits[i] & positivemask) == 0 && info[(i+1)*8 - n]%2 ==1)
+			{
+				// Then we need to remove 1 to info by And'ing a 1111 1110
+				info[(i+1)*8 - n] = (byte) (info[(i+1)*8 - n] & 0xFE);
+				System.out.println("We added a 0 at Index: "+((i+1)*8 - n));
+			}
+			else
+			{
+				System.out.println("There is a "+(info[(i+1)*8 - n] & 0x01)+" at Index: "+((i+1)*8 - n));
+			}
+		}
+	}	
 }
 
 public String recoverData()
 {
-	// DecodeImage decoder = new DecodeImage(this);
-	return "";
+	// Work On Info to rebuild the characters from the LSD's
+	String ret = "";
+	// Get LSD's in groups of 8
+	/*
+	char[] LSDs = new char[8];
+	Boolean end = false;
+		for(int i =0; i <info.length && !end;i++)
+		{
+			if(i != 0 && i%8 ==0)// Have reached the beginning of a new Character
+			{
+				char add = 0;
+				for(int j=0; j<LSDs.length;j++)
+				{
+					add += (int) (LSDs[j] << (7-j));
+					System.out.print((int) LSDs[j]);
+				}
+				System.out.println();
+				ret.concat( String.valueOf(add));
+				if(add == '\0')
+				{
+					end = true;
+				}
+				LSDs[0] = (char) (info[i] & 0x01);
+			}
+			else
+			{
+				LSDs[i%8] = (char) (info[i] & 0x01);
+			}
+		}*/
+		
+	Boolean end = false;
+	int multiplier = 0;
+	while(!end)
+	{
+	char add = 0x00;
+	add+= (int)( (info[0+(multiplier*8)] & 0x01) << 7);
+	add+= (int)( (info[1+(multiplier*8)] & 0x01) << 6);
+	add+= (int)( (info[2+(multiplier*8)] & 0x01) << 5);
+	add+= (int)( (info[3+(multiplier*8)] & 0x01) << 4);
+	add+= (int)( (info[4+(multiplier*8)] & 0x01) << 3);
+	add+= (int)( (info[5+(multiplier*8)] & 0x01) << 2);
+	add+= (int)( (info[6+(multiplier*8)] & 0x01) << 1);
+	add+= (int)( (info[7+(multiplier*8)] & 0x01));
+	multiplier++;
+	System.out.println(add);
+	if(add == 0x00)
+	{
+		end=true;
+	}
+	else
+	{
+		ret += ""+add+"";
+	}
+	}
+	
+	return ret;
+	
 }
+
+
 
 }
